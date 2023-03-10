4919bf0722e5ecd8bdefaa75b80eb5f2541d3bc5
LuannBarroso <luan23.lba@gmail.com>
2023-03-09 23:58:53 -0400

    created document string

---
+++ lib/strings/strings.dart
@@ -0,0 +1,4 @@
+class StringsText {
+  static const String text =
+      ' lorem ipsum dolor sit amet, consectur adipicning elit. \nDiam maecesanas ,i non sed ut odio. Non, justo, sed dacilisi \n Et. Eget viverra urna, vestibulum egestas faucibus \n egestas. Sagittis nam velit volutpat eu nunc.  ';
+}
