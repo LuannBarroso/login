ec95fc0c2a35cfb1988097e3ea41e03dade14b32
LuannBarroso <luan23.lba@gmail.com>
2023-03-12 21:09:22 -0400

    correcting text layout

--- lib/strings/strings.dart
+++ lib/strings/strings.dart
@@ -1,4 +1,4 @@
 class StringsText {
   static const String text =
-      ' lorem ipsum dolor sit amet, consectur adipicning elit. \nDiam maecesanas ,i non sed ut odio. Non, justo, sed dacilisi \n Et. Eget viverra urna, vestibulum egestas faucibus \n egestas. Sagittis nam velit volutpat eu nunc.  ';
+      '     Lorem ipsum dolor sit amet, consectur adipicning elit.\nDiam maecesanas ,i non sed ut odio. Non, justo, sed dacilisi \n       et. Eget viverra urna, vestibulum egestas faucibus        \n             egestas. Sagittis nam velit volutpat eu nunc.  ';
 }
