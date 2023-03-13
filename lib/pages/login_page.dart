40b586476eed7a9e50ba9e9db626e000ca5171f8
LuannBarroso <luan23.lba@gmail.com>
2023-03-13 01:02:54 -0400

    Add textfild

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,17 +12,54 @@ class LoginPage extends StatelessWidget {
             padding: const EdgeInsets.all(50.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                Center(
+              children: [
+                const Center(
                   child: Text(
                     'Welcome Back',
                     style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Center(
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Center(
                   child: Text(
-                    'Lorem ',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    'Lorem ipsum dolor sit amet, consectetur adipscing \nelit. Diam maecenas mi non sed ut odio. Non, justo, \n                                    sed facilisi et. ',
+                    style: TextStyle(
+                      fontSize: 13.0,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                Container(
+                  height: 59,
+                  width: 339,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: const Color.fromRGBO(243, 243, 243, 1),
+                  ),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'User name , Email & Phone Number',
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  height: 59,
+                  width: 339,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: const Color.fromRGBO(243, 243, 243, 1),
+                  ),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
                   ),
                 ),
               ],
