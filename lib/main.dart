08e872d9113e3377138829e441d1f737dbf52430
LuannBarroso <luan23.lba@gmail.com>
2023-03-12 21:09:52 -0400

    correcting name in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login/home_page.dart';
+import 'package:login/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: const HomePage(),
     );
   }
 }
