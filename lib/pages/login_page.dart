9bf79e607143d3d8dd647786a02f0c67c459386a
LuannBarroso <luan23.lba@gmail.com>
2023-03-14 17:24:38 -0400

    feat: create cicle facebook

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -190,6 +190,22 @@ class LoginPage extends StatelessWidget {
                   const SizedBox(
                     width: 16,
                   ),
+                  Container(
+                    height: 52,
+                    width: 52,
+                    padding: const EdgeInsets.all(6.0),
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 238, 230, 230),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.asset(
+                      'assets/images/png/facebook=.png',
+                      scale: 46,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
                 ],
               ),
             ],
