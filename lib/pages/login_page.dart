1eba2afe47ecb04a909fee9842419e7a1481b02e
LuannBarroso <luan23.lba@gmail.com>
2023-03-14 17:22:34 -0400

    feat: create login bottom

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,64 +6,172 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Center(
-                  child: Text(
-                    'Welcome Back',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 50.0),
+          child: Column(
+            children: [
+              const Center(
+                child: Text(
+                  'Welcome Back',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(
+                height: 4.0,
+              ),
+              const Center(
+                child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipscing \nelit. Diam maecenas mi non sed ut odio. Non, justo, \n                                    sed facilisi et. ',
+                  style: TextStyle(
+                    fontSize: 13.0,
                   ),
                 ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                const Center(
-                  child: Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipscing \nelit. Diam maecenas mi non sed ut odio. Non, justo, \n                                    sed facilisi et. ',
-                    style: TextStyle(
-                      fontSize: 13.0,
+              ),
+              const SizedBox(
+                height: 26.0,
+              ),
+              Container(
+                height: 59,
+                width: 339,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: const Color.fromARGB(255, 238, 230, 230),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      offset: const Offset(0, 2),
                     ),
-                  ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 50.0,
-                ),
-                Container(
-                  height: 59,
-                  width: 339,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15.0),
-                    color: const Color.fromRGBO(243, 243, 243, 1),
-                  ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: TextFormField(
                     decoration: const InputDecoration(
-                      labelText: 'User name , Email & Phone Number',
+                      labelText: 'Username , Email & Phone Number',
+                      border: InputBorder.none,
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 10.0,
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                height: 59,
+                width: 339,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: const Color.fromARGB(255, 238, 230, 230),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
                 ),
-                Container(
-                  height: 59,
-                  width: 339,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15.0),
-                    color: const Color.fromRGBO(243, 243, 243, 1),
-                  ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: TextFormField(
                     decoration: const InputDecoration(
                       labelText: 'Password',
+                      border: InputBorder.none,
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 30),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Container(
+                height: 59,
+                width: 339,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: const Color.fromRGBO(248, 154, 238, 1),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                      height: 3,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(248, 154, 238, 1),
+                            Color.fromRGBO(196, 196, 196, 0)
+                          ],
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Text(
+                    'Or Sign up With',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                      height: 3,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(248, 154, 238, 1),
+                            Color.fromRGBO(196, 196, 196, 0)
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 36,
+              ),
+            ],
           ),
         ),
       ),
